43171c3423835dc5f404ac323cc61dd509885185
ZippyDaFish <andrew.riker21@gmail.com>
2022-09-03 15:41:25 -0400

    Add comments for clarity

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ function Book(title, author, pageNum, read){
     this.pageNum = pageNum;
     this.read = read;
 }
-
+// creates a book object and adds it to myLibrary
 function addBook(title, author, pageNum, read){
     let newBook = new Book(title, author, pageNum, read);
     myLibrary.push(newBook);
 }
-
+// creates card with info of given book object
 function createCard(b) {
     var div = document.createElement('div');
     div.setAttribute('class', 'book-card text-main');
@@ -32,10 +32,13 @@ myLibrary.forEach(b => console.log(b)); //log books to console
 document.getElementById('grid-main').innerHTML = "";
 myLibrary.forEach(b => createCard(b)); // create a card for each book in the library
 
+// event listener for adding a new book
 const addBtn = document.getElementById('add-book');
 addBtn.addEventListener('click', function() {
     document.getElementById('form-wrap').style.display = 'flex';
 });
+
+// form open-close toggling
 const formWrap = document.getElementById('form-wrap');
 formWrap.addEventListener('click', function(){
     const form = document.getElementById('add-form');
