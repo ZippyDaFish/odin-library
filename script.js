c73c382262437d621f28f8583b5c72e9e05691a0
ZippyDaFish <andrew.riker21@gmail.com>
2022-09-04 21:28:40 -0400

    Begin card removal

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 let myLibrary = [];
 
-function Book(title, author, pageNum, read){
+function Book(title, author, pageNum, read, index){
     this.title = title;
     this.author = author;
     this.pageNum = pageNum;
     this.read = read;
+    this.index = index;
 }
 // creates a book object and adds it to myLibrary
 function addBook(title, author, pageNum, read){
-    let newBook = new Book(title, author, pageNum, read);
+    let newBook = new Book(title, author, pageNum, read, myLibrary.length);
     myLibrary.push(newBook);
     refreshCards();
 }
@@ -18,35 +19,37 @@ function bookSubmit(){
     var p = document.getElementById('page-num').value;
     var r;
     var check = document.getElementById('read');
-    if(check.checked == true){ r = true;}
-    else if(check.check != true){ r = false;}
-    console.log(t, auth, p, r);
+    if(check.checked == true){ r = "Read";}
+    else if(check.check != true){ r = "Unread";}
     addBook(t, auth, p, r);
 }
 // creates card with info of given book object
-function createCard(b) {
+function createCard(b, remIndex) {
     var div = document.createElement('div');
     div.setAttribute('class', 'book-card text-main');
     div.innerHTML = `
     <p>${b.title}</p>
     <p>${b.author}</p>
     <p>${b.pageNum}</p>
-    <button class='text-main button-main'>Read/Unread</button>
-    <button class='text-main button-main'>Remove</button>
+    <button class='text-main button-main'>${b.read}</button>
+    <button class='text-main button-main' onclick='removeCard(${remIndex})'>${remIndex}</button>
     `;
     document.getElementById('grid-main').appendChild(div);
 }
+function removeCard(remIndex){
+    myLibrary.splice(remIndex, 1);
+    refreshCards();
+    console.log(remIndex);
+}
 function refreshCards() {
     document.getElementById('grid-main').innerHTML = "";
-    myLibrary.forEach(b => createCard(b)); // create a card for each book in the library
+    myLibrary.forEach(b => createCard(b, b.index)); // create a card for each book in the library
 }
 
 function showForm(show){
     if(show){ document.getElementById('form-wrap').style.display = 'flex'; }
     else if(!show){ document.getElementById('form-wrap').style.display = 'none'; }
 }
-
-myLibrary.forEach(b => console.log(b)); //log books to console
 
 // event listener for adding a new book
 const addBtn = document.getElementById('add-book');
